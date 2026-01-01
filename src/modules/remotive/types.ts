61bd7dfc1a513def542aff01105ceb9ea859be0e
Erick Vargas <mail@erian.dev>
2026-01-01 18:59:22 -0500

    refactor(remotive): update types

--- src/modules/remotive/types.ts
+++ src/modules/remotive/types.ts
@@ -1,9 +1,10 @@
 export type CategorizableJobAtrrs = keyof Pick<Job, 'category' | 'job_type'>;
 export interface Job {
-  id: string;
+  id: number;
   url: string;
   title: string;
   company_name: string;
+  company_logo: string;
   category: string;
   tags: string[];
   job_type: string;
@@ -14,7 +15,9 @@ export interface Job {
   company_logo_url: string;
 }
 export interface JobsListResponse {
+  '00-warning': string;
   '0-legal-notice': string;
   'job-count': number;
+  'total-job-count': number;
   jobs: Job[];
 }
