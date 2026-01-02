6c19de6e76ef1a7a5618f715137bb515e5a25bf0
Erick Vargas <mail@erian.dev>
2026-01-02 15:53:58 -0500

    refactor(shared): remove hours, minutes & seconds from the timestamp

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,9 +1,8 @@
 export function getTimeStamp(date = new Date()) {
   const CURRENT_DATE = date.getDate().toString().padStart(2, '00')
   const CURRENT_MONTH = (date.getMonth() + 1).toString().padStart(2, '00')
-  const CURRENT_TIME = `${date.getHours()}:${date.getMinutes().toString().padStart(2, '00')}`
 
-  return `${CURRENT_DATE}/${CURRENT_MONTH}/${date.getFullYear()} ${CURRENT_TIME} GTM-0500`
+  return `${CURRENT_DATE}/${CURRENT_MONTH}/${date.getFullYear()}`
 }
 
 export function slugify(text: string) {
