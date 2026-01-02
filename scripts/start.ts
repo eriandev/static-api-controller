fee8d032cc23f3a037e3be6411c35150b185160d
Erick Vargas <mail@erian.dev>
2026-01-02 15:48:35 -0500

    refactor: update deno tasks & scripts

--- scripts/start.ts
+++ scripts/start.ts
@@ -1,14 +1,15 @@
+const PERMISSIONS = [
+  '--allow-sys',
+  '--allow-env',
+  '--allow-read',
+  '--allow-write',
+  '--allow-run',
+  '--allow-net=github.com,api.github.com,remotive.io,remotive.com',
+] as const
+
 async function main() {
-  const PERMISSIONS = [
-    '--allow-sys',
-    '--allow-env',
-    '--allow-read',
-    '--allow-write',
-    '--allow-run',
-    '--allow-net=github.com,api.github.com,remotive.io',
-  ]
   const command = new Deno.Command('deno', {
-    args: ['run', ...PERMISSIONS, 'src/main.ts'],
+    args: ['run', '--env', ...PERMISSIONS, 'src/main.ts'],
     stdin: 'inherit',
     stdout: 'inherit',
   })
